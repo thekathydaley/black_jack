5fa44e156b636afd9b4845c4398af23d7be110da
thekathydaley <thekathydaley@gmail.com>
2021-04-15 14:25:17 -0400

    third commit

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 console.log
-var 
-var
-var
+// Building my deck with Variables
+// Create the four suits
+var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+// Assign the card values
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+// Creates an open array for the changing card variable, pairs up each var above
+var deck = new Array ();
+
+// Functions
+
+function createDeck()
+//function to produce a new deck
+{   
+    deck = new Array();
+    for(var i = 0 ; i < values.length; i++)
+{
+    for(var x = 0; x < suits.length)
+}    
+
+}
