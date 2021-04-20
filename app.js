75e283d6604969a6ad73c619304856413fafe0bf
thekathydaley <thekathydaley@gmail.com>
2021-04-19 21:48:58 -0400

    eleventh commit

--- app.js
+++ app.js
@@ -1,40 +1,34 @@
-console.log
 // Building my deck with Variables
 // Create the four suits
 var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 // Assign the card values
 var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 // Creates an open array for the changing card variable, pairs up each var above
-var deck = new Array ();
+var deck = new Array();
+var players = new Array();
+var currentPlayer = 0;
 
 // Functions
-
 function createDeck()
 //function to produce a new deck
-{   
+{
     deck = new Array();
-    for(var i = 0 ; i < values.length; i++)
-{
-    for(var x = 0; x < suits.length; x++)
-    {
-        //change the letters to be considered numbers in the array and assign facecard values
-        var weight = parseInt(values[i]);
-                if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
-                    weight = 10;
-                if (values[i] == "A")
-                    weight = 11;
-                var card = { Value: values[i], Suit: suits[x], Weight: weight };
-                deck.push(card);
-
+    for (var i = 0; i < values.length; i++) {
+        for (var x = 0; x < suits.length; x++) {
+            var weight = parseInt(values[i]);
+            if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
+                weight = 10;
+            if (values[i] == "A")
+                weight = 11;
+            var card = { Value: values[i], Suit: suits[x], Weight: weight };
+            deck.push(card);
+        }
     }
-}    
-
 }
+
 // Shuffle Function - set to loop 1000 times to make sure it's mostly random
-function shuffle()
-{
-    for (var i = 0; i < 1000; 1++)
-    {
+function shuffle() {
+    for (var i = 0; i < 1000; i++) {
         var location1 = Math.floor((Math.random() * deck.length));
         var location2 = Math.floor((Math.random() * deck.length));
         var tmp = deck[location1];
@@ -46,23 +40,18 @@ function shuffle()
 
 // Players
 
-var players = new Array();
-function createPlayers(num)
-{
+function createPlayers(num) {
     players = new Array();
-    for(var i = 1; i <= num; i++)
-    {
+    for (var i = 1; i <= num; i++) {
         var hand = new Array();
         var player = { Name: 'Player ' + i, ID: i, Points: 0, Hand: hand };
         players.push(player);
     }
 }
 
-function createPlayersUI()
-{
+function createPlayersUI() {
     document.getElementById('players').innerHTML = '';
-    for(var i = 0; i < players.length; i++)
-    {
+    for (var i = 0; i < players.length; i++) {
         var div_player = document.createElement('div');
         var div_playerid = document.createElement('div');
         var div_hand = document.createElement('div');
@@ -74,7 +63,7 @@ function createPlayersUI()
         div_player.className = 'player';
         div_hand.id = 'hand_' + i;
 
-        div_playerid.innerHTML = players[i].ID;
+        div_playerid.innerHTML = 'Player ' + players[i].ID;
         div_player.appendChild(div_playerid);
         div_player.appendChild(div_hand);
         div_player.appendChild(div_points);
@@ -82,11 +71,11 @@ function createPlayersUI()
     }
 }
 
+
 // Beginning the game
-function startBlackJack()
-{
+function startblackjack() {
     document.getElementById('btnStart').value = 'Restart';
-    document.getElementById('status').style.display="none";
+    document.getElementById("status").style.display = "none";
     currentPlayer = 0;
     createDeck();
     shuffle();
@@ -95,103 +84,128 @@ function startBlackJack()
     dealHands();
     document.getElementById('player_' + currentPlayer).classList.add('active');
 }
+
 // Dealing the cards
 // one card to each player, pop will help select the card from the array above
 //then the push will assign one card to each player
-function dealHands()
-{
-    for (var i = 0; i < 2; i++)
-    {
-        for (var x = 0; x < players.length; x++)
-    {
-        var card = desk.pop();
-        players[x].Hand.push(card);
-        addCard(card, x);
-        updatePoints();
+function dealHands() {
+    // alternate handing cards to each player
+    // 2 cards each
+    for (var i = 0; i < 2; i++) {
+        for (var x = 0; x < players.length; x++) {
+            var card = deck.pop();
+            players[x].Hand.push(card);
+            renderCard(card, x);
+            updatePoints();
+        }
     }
-    }
+
     updateDeck();
 }
+
 // functions to put the card in the player's hand - research harder on this  
 // -  the functions are confusing
-function addCard( card, player)
-{
+function renderCard(card, player) {
     var hand = document.getElementById('hand_' + player);
     hand.appendChild(getCardUI(card));
 }
-function getCardUI(card)
-{
+function getCardUI(card) {
     var el = document.createElement('div');
+    var icon = '';
+    if (card.Suit == 'Hearts')
+        icon = '&hearts;';
+    else if (card.Suit == 'Spades')
+        icon = '&spades;';
+    else if (card.Suit == 'Diamonds')
+        icon = '&diams;';
+    else
+        icon = '&clubs;';
+
     el.className = 'card';
-    el.innerHTML = card.Suit + ' '+ card.Value;
+    el.innerHTML = card.Value + '<br/>' + icon;
     return el;
 }
+
+
+// returns the number of points that a player has in hand
+function getPoints(player) {
+    var points = 0;
+    for (var i = 0; i < players[player].Hand.length; i++) {
+        points += players[player].Hand[i].Weight;
+    }
+    players[player].Points = points;
+    return points;
+}
+
+function updatePoints() {
+    for (var i = 0; i < players.length; i++) {
+        getPoints(i);
+        document.getElementById('points_' + i).innerHTML = players[i].Points;
+    }
+}
+
 // HIT ME FUNCTION - POPs card out of array and adds to player's cards
-var currentPlayer = 0; 
-function hitMe()
-{
-    var card = deck-pop();
+function hitMe() {
+    var card = deck.pop();
     players[currentPlayer].Hand.push(card);
-    addCard(card, currentPlayer);
+    renderCard(card, currentPlayer);
     updatePoints();
+    updateDeck();
     check();
 }
 //function to check if cards exceed 21
-function check()
-{
-    if (players[currentPlayer].Points > 21)
-    {
-        document.getElementById('status').innerHTML= 'Player: ' + players[currentPlayer].ID + ' LOST';
+function check() {
+    if (players[currentPlayer].Points > 21) {
+        document.getElementById('status').innerHTML = 'Player: ' + players[currentPlayer].ID + ' LOST';
     }
 }
 
 // HOLD/STAY FUNCTION
 
-function stay()
-    {
-        if (currentPlayer != players.length-1){
-            document.getElementById('player_' + currentPlayer).classList.remove('active');
-            currentPlayer += 1;
-            document.getElementById('player_' + currentPlayer).classList.add('active');
-    }
-        else {
-            end();
-        }
+function stay() {
+    if (currentPlayer != players.length - 1) {
+        document.getElementById('player_' + currentPlayer).classList.remove('active');
+        currentPlayer += 1;
+        document.getElementById('player_' + currentPlayer).classList.add('active');
     }
 
-function end()
-{
+    else {
+        end();
+    }
+}
+
+
+function end() {
     var winner = -1;
     var score = 0;
 
-    for (var i = 0; i < players.length; i++)
-    {
-        if (players[i].Points > score && players[i].Points < 22)
-        {
+    for (var i = 0; i < players.length; i++) {
+        if (players[i].Points > score && players[i].Points < 22) {
             winner = i;
         }
+
         score = players[i].Points;
     }
-    document.getElementById('status').innerHTML = "Player " + players[winner].ID + " wins!"
+
+    document.getElementById('status').innerHTML = 'Winner: Player ' + players[winner].ID;
     document.getElementById("status").style.display = "inline-block";
 }
-function check()
-{
-    if (players[currentPlayer].Points > 21)
-    {
+
+function check() {
+    if (players[currentPlayer].Points > 21) {
         document.getElementById('status').innerHTML = 'Player: ' + players[currentPlayer].ID + ' LOST';
         document.getElementById('status').style.display = "inline-block";
         end();
     }
 }
 
-function updateDeck()
-{
-    document.getElementById('deckcount').innerHTML = deck.length;
+function updateDeck() {
+    document.getElementById('deckCount').innerHTML = deck.length;
 }
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     createDeck();
     shuffle();
     createPlayers(1);
 });
+console.log
